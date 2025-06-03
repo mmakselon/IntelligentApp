64af0236393ed190867b271c0b4add8d3cd8a34f
mmakselon <magdalena.pachacz@gmail.com>
2025-06-03 21:42:28 +0200

    add ChatCompletionResponse

---
+++ IntelligentApp/Models/ChatCompletionResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace IntelligentApp.Models
+{
+    public class ChatCompletionResponse
+    {
+        public string Id { get; set; }
+        public string Object { get; set; }
+        public long Created { get; set; }
+        public List<ChatCompletionChoice> Choices { get; set; }
+        public ChatCompletionUsage Usage { get; set; }
+    }
+}
