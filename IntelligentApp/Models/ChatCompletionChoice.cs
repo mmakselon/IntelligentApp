28a5bb5f596a6488a34656c124714866f1d80325
mmakselon <magdalena.pachacz@gmail.com>
2025-06-05 23:17:27 +0200

    add new class

---
+++ IntelligentApp/Models/ChatCompletionChoice.cs
@@ -0,0 +1,9 @@
+﻿namespace IntelligentApp.Models
+{
+    public class ChatCompletionChoice
+    {
+        public int Index { get; set; }
+        public ChatCompletionMessage Message { get; set; }
+        public string FinishReason { get; set; }
+    }
+}
---
+++ IntelligentApp/Models/ChatCompletionUsage.cs
@@ -0,0 +1,9 @@
+﻿namespace IntelligentApp.Models
+{
+    public class ChatCompletionUsage
+    {
+        public int PromptTokens { get; set; }
+        public int CompletionTokens { get; set; }
+        public int TotalTokens { get; set; }
+    }
+}
