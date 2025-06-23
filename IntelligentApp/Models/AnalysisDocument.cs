f9a17a3876008df4e5e17773e7c1bc061ed7bb6a
mmakselon <magdalena.pachacz@gmail.com>
2025-06-23 22:14:44 +0200

    add Model AnalysisDocument

---
+++ IntelligentApp/Models/AnalysisDocument.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace IntelligentApp.Models
+{
+    public class AnalysisDocument
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("language")]
+        public string Language { get; set; }
+
+        [JsonPropertyName("text")]
+        public string Text { get; set; }
+    }
+}
