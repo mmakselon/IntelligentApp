6b623ed7c70ae6b4a64ca8be242ab78065aa47c6
mmakselon <magdalena.pachacz@gmail.com>
2025-06-23 22:14:01 +0200

    add Model AnalysisInput

---
+++ IntelligentApp/Models/AnalysisInput.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace IntelligentApp.Models
+{
+    public class AnalysisInput
+    {
+        [JsonPropertyName("documents")]
+        public List<AnalysisDocument> Documents { get; set; }
+    }
+}
