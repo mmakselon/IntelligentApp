f4b8c29b82506e0439d6ee22322ec6f14eacca34
mmakselon <magdalena.pachacz@gmail.com>
2025-06-26 22:33:11 +0200

    add Model AnalyseTextDocument

---
+++ IntelligentApp/Models/AnalyzeTextDocument.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace IntelligentApp.Models
+{
+    public class AnalyzeTextDocument
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("sentiment")]
+        public string Sentiment { get; set; }
+
+        [JsonPropertyName("confidenceScores")]
+        public ConfidenceScores ConfidenceScores { get; set; }
+
+        [JsonPropertyName("keyPhrases")]
+        public List<string> KeyPhrases { get; set; }
+    }
+}
