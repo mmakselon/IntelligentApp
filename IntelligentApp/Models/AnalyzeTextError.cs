01495eab221ef059e7f505a83dd5f0a6c81b99a7
mmakselon <magdalena.pachacz@gmail.com>
2025-07-03 22:39:05 +0200

    add AnalyzeTextError

---
+++ IntelligentApp/Models/AnalyzeTextError.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace IntelligentApp.Models
+{
+    public class AnalyzeTextError
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("error")]
+        public ErrorDetails Error { get; set; }
+    }
+}
