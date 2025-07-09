6f294fc293c77947ae37cb78bba2d6d94d548aab
mmakselon <magdalena.pachacz@gmail.com>
2025-07-09 21:21:15 +0200

    add AnalyzeTextRequest

---
+++ IntelligentApp/Models/AnalyzeTextRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace IntelligentApp.Models
+{
+    public class AnalyzeTextRequest
+    {
+        [JsonPropertyName("kind")]
+        public string Kind { get; set; }
+
+        [JsonPropertyName("analysisInput")]
+        public AnalysisInput AnalysisInput { get; set; }
+
+        [JsonPropertyName("parameters")]
+        public Dictionary<string, object> Parameters { get; set; }
+    }
+}
