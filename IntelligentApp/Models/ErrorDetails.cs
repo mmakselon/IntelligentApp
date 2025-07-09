30c15a05b61033829bfdfa927ae21fa4dd1077a7
mmakselon <magdalena.pachacz@gmail.com>
2025-07-09 21:47:42 +0200

    add ErrorDetails

---
+++ IntelligentApp/Models/ErrorDetails.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace IntelligentApp.Models
+{
+    public class ErrorDetails
+    {
+        [JsonPropertyName("code")]
+        public string Code { get; set; }
+
+        [JsonPropertyName("message")]
+        public string Message { get; set; }
+    }
+}
