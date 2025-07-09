a40ad41dcac85d376c29900205981e830ef13860
mmakselon <magdalena.pachacz@gmail.com>
2025-07-09 21:35:17 +0200

    add AnalyzeTextResponse

---
+++ IntelligentApp/Models/AnalyzeTextResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace IntelligentApp.Models
+{
+    public class AnalyzeTextResponse
+    {
+        [JsonPropertyName("kind")]
+        public string Kind { get; set; }
+
+        [JsonPropertyName("results")]
+        public AnalyzeTextResults Results { get; set; }
+    }
+}
