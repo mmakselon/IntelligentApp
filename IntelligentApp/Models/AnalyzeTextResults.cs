a02594888268a963b34126f7f544f5beded51154
mmakselon <magdalena.pachacz@gmail.com>
2025-07-09 21:35:48 +0200

    add AnalyzeTextResult

---
+++ IntelligentApp/Models/AnalyzeTextResults.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace IntelligentApp.Models
+{
+    public class AnalyzeTextResults
+    {
+        [JsonPropertyName("documents")]
+        public List<AnalyzeTextDocument> Documents { get; set; }
+
+        [JsonPropertyName("errors")]
+        public List<AnalyzeTextError> Errors { get; set; }
+
+        [JsonPropertyName("modelVersion")]
+        public string ModelVersion { get; set; }
+    }
+}
