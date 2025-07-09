7fb034bd0a7fa9220b1a872b057b6b959243b27a
mmakselon <magdalena.pachacz@gmail.com>
2025-07-09 21:43:57 +0200

    add ConfidenceScores

---
+++ IntelligentApp/Models/ConfidenceScores.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace IntelligentApp.Models
+{
+    public class ConfidenceScores
+    {
+        [JsonPropertyName("positive")]
+        public double Positive { get; set; }
+
+        [JsonPropertyName("neutral")]
+        public double Neutral { get; set; }
+
+        [JsonPropertyName("negative")]
+        public double Negative { get; set; }
+    }
+}
